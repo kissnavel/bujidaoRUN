62259e89d75f1eb84920e2782092f7290efada20
kissnavel <xkn0722@gmail.com>
2025-02-02 21:38:58 +0800

    fix:星铁预估

--- apps/estimate.js
+++ apps/estimate.js
@@ -1,5 +1,5 @@
 /* 原石预估来自米游社@要和我一起去炸鱼吗m */
-/* 星琼预估来自米游社@乌鸦Rabble */
+/* 星琼预估来自米游社@弃剑不负你 */
 import fetch from 'node-fetch'
 import common from '../../../lib/common/common.js'
 import Cfg from '../model/Cfg.js'
@@ -25,7 +25,7 @@ export class ji_estimate extends plugin {
     let res
     const isSr = /星铁|星琼/.test(this.e.msg)
     if (isSr) {
-      res = await (await fetch("https://bbs-api.miyoushe.com/painter/api/user_instant/search/list?keyword=%E5%8F%AF%E8%8E%B7%E5%8F%96%E6%98%9F%E7%90%BC&uid=73779489&size=20&offset=0&sort_type=2")).json()
+      res = await (await fetch("https://bbs-api.miyoushe.com/painter/api/user_instant/search/list?keyword=%E5%8F%AF%E8%8E%B7%E5%8F%96%E6%98%9F%E7%90%BC&uid=286192908&size=20&offset=0&sort_type=2")).json()
     } else {
       res = await (await fetch("https://bbs-api.miyoushe.com/painter/api/user_instant/search/list?keyword=%E5%8F%AF%E8%8E%B7%E5%8F%96%E5%8E%9F%E7%9F%B3&uid=288721427&size=20&offset=0&sort_type=2")).json()
     }
