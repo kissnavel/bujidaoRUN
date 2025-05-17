33d0aecae5fefcdf7233c15315179e6970a5f5b2
kissnavel <xkn0722@gmail.com>
2025-05-17 15:21:34 +0800

    fix:请求头

--- model/mys/mysApi.js
+++ model/mys/mysApi.js
@@ -187,15 +187,9 @@ export default class MysApi {
     }
 
     const device_fp = await redis.get(`genshin:device_fp:${ltuid}:fp`)
-    if (device_fp) {
-      data.deviceFp = device_fp
-      data.headers['x-rpc-device_fp'] = device_fp
-    }
+    if (device_fp) data.deviceFp = device_fp
     const device_id = await redis.get(`genshin:device_fp:${ltuid}:id`)
-    if (device_id) {
-      data.deviceId = device_id
-      data.headers['x-rpc-device_id'] = device_id
-    }
+    if (device_id) data.deviceId = device_id
 
     if (game) this.game = game
     let { url, headers, body, config } = this.getUrl(type, data)
