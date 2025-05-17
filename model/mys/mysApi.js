02d0f32452aeea5510bb71ef458e7ed00c7a9bab
kissnavel <xkn0722@gmail.com>
2025-05-17 15:09:27 +0800

    fix:调整顺序

--- model/mys/mysApi.js
+++ model/mys/mysApi.js
@@ -156,42 +156,45 @@ export default class MysApi {
   async getData(type, data = {}, game = '', cached = false) {
     const ck = this.cookie
     const ltuid = ck.ltuid
+    if (ltuid) {
+      let bindInfo = await redis.get(`genshin:device_fp:${ltuid}:bind`)
+      if (bindInfo) {
+        try {
+          bindInfo = JSON.parse(bindInfo)
+          data = {
+            ...data,
+            productName: bindInfo?.deviceProduct,
+            deviceType: bindInfo?.deviceName,
+            modelName: bindInfo?.deviceModel,
+            oaid: bindInfo?.oaid,
+            osVersion: bindInfo?.androidVersion,
+            deviceInfo: bindInfo?.deviceFingerprint,
+            board: bindInfo?.deviceBoard
+          }
+        } catch (error) {
+          bindInfo = null
+        }
+      }
+    }
     if (this.types.includes(type)) {
       if (!this._device_fp && !data?.Getfp && !data?.headers?.['x-rpc-device_fp']) {
-        this._device_fp = await this.getData('getFp', { Getfp: true })
+        this._device_fp = await this.getData('getFp', {
+          ...data,
+          Getfp: true
+        })
       }
       if (type === 'getFp' && !data?.Getfp) return this._device_fp
+    }
 
-      if (ltuid) {
-        let bindInfo = await redis.get(`genshin:device_fp:${ltuid}:bind`)
-        if (bindInfo) {
-          try {
-            bindInfo = JSON.parse(bindInfo)
-            data = {
-              ...data,
-              productName: bindInfo?.deviceProduct,
-              deviceType: bindInfo?.deviceName,
-              modelName: bindInfo?.deviceModel,
-              oaid: bindInfo?.oaid,
-              osVersion: bindInfo?.androidVersion,
-              deviceInfo: bindInfo?.deviceFingerprint,
-              board: bindInfo?.deviceBoard
-            }
-          } catch (error) {
-            bindInfo = null
-          }
-        }
-        const device_fp = await redis.get(`genshin:device_fp:${ltuid}:fp`)
-        if (device_fp) {
-          data.deviceFp = device_fp
-          data.headers['x-rpc-device_fp'] = device_fp
-        }
-        const device_id = await redis.get(`genshin:device_fp:${ltuid}:id`)
-        if (device_id) {
-          data.deviceId = device_id
-          data.headers['x-rpc-device_id'] = device_id
-        }
-      }
+    const device_fp = await redis.get(`genshin:device_fp:${ltuid}:fp`)
+    if (device_fp) {
+      data.deviceFp = device_fp
+      data.headers['x-rpc-device_fp'] = device_fp
+    }
+    const device_id = await redis.get(`genshin:device_fp:${ltuid}:id`)
+    if (device_id) {
+      data.deviceId = device_id
+      data.headers['x-rpc-device_id'] = device_id
     }
 
     if (game) this.game = game
