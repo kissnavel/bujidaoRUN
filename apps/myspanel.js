f04796bd0587d08c77548673dde3c52a5e99c7d0
kissnavel <xkn0722@gmail.com>
2025-05-24 20:35:13 +0800

    feat:通过uid或at查询米游社面板

--- apps/myspanel.js
+++ apps/myspanel.js
@@ -4,6 +4,7 @@ import plugin from '../../../lib/plugins/plugin.js'
 import ProfileList from '../model/ProfileList.js'
 import Myspanel from '../model/myspanel.js'
 import MysInfo from '../model/mys/mysInfo.js'
+import MysApi from '../model/mys/mysApi.js'
 import Cfg from '../model/Cfg.js'
 import moment from 'moment'
 
@@ -15,21 +16,36 @@ export class ji_myspanel extends plugin {
             event: 'message',
             priority: Cfg.getConfig('config').priority,
             rule: [{
-                reg: '^#?(原神|星铁)?(寄|米游社|mys)?(全部面板更新|更新全部面板|获取游戏角色详情|更新面板|面板更新)$',
+                reg: '^#?(原神|星铁)?(寄|米游社|mys)?(全部面板更新|更新全部面板|获取游戏角色详情|更新面板|面板更新)',
                 fnc: 'mys'
             }]
-        });
+        })
     }
 
     async mys(e) {
         let panel = Cfg.getConfig('config').myspanel
         if (!panel) return false
-        let User = await MysInfo.get(e, 'UserGame', {}, {}, true)
-        if (User?.retcode !== 0) return false
-        let CD = Cfg.getConfig('config').myspanelCD
-        let uid = await MysInfo.getUid(e, false)
-        if (!uid) return e.reply('找不到uid，请：#刷新ck 或者：#扫码登录', true)
 
+        let user = e.user_id
+        let ats = e.message.filter(m => m.type === 'at')
+        if (ats.length > 0 && !e.atBot) {
+            user = ats[0].qq
+            e.user_id = user
+        }
+        let uid = e.msg.match(/\d+/)?.[0] || await MysInfo.getUid(e, false)
+        if (!uid) {
+            await e.reply('找不到uid，请：#刷新ck 或者：#扫码登录', true)
+            return false
+        }
+        let game = e.game
+        let ck = await MysInfo.checkUidBing(uid, game)
+        ck = ck.ck
+        if (!ck) {
+            await e.reply(`uid:${uid}当前尚未绑定Cookie`)
+            return false
+        }
+
+        let CD = Cfg.getConfig('config').myspanelCD
         let now
         if (CD > 0) {
             now = moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
@@ -41,19 +57,19 @@ export class ji_myspanel extends plugin {
         }
 
         await e.reply(`开始查询uid:${uid}的米游社面板数据，可能会需要一定时间~`, true)
-        let device_fp = await MysInfo.get(e, 'getFp', {}, {}, true)
-        if (device_fp?.retcode !== 0) return false
+        let mysApi = new MysApi(uid, ck, {}, game)
+        let device_fp = await mysApi.getData('getFp')
         let headers = { 'x-rpc-device_fp': device_fp?.data?.device_fp }
         let res, data
-        if (e.game == 'sr') {
-            data = await MysInfo.get(e, 'avatarInfo', { headers }, {}, true)
+        if (game == 'sr') {
+            data = await mysApi.getData('avatarInfo', { headers })
             if (!data.data) {
                 logger.mark('米游社查询失败')
                 return false
             }
             await Myspanel.sr_mys(data, uid)
         } else {
-            res = await MysInfo.get(e, 'character', { headers }, {}, true)
+            res = await mysApi.getData('character', { headers })
             if (!res.data) {
                 logger.mark('米游社查询失败')
                 return false
@@ -62,7 +78,7 @@ export class ji_myspanel extends plugin {
             res.data.list.map((value) => {
                 ids.push(value.id)
             })
-            data = await MysInfo.get(e, 'character_detail', { headers, ids: ids }, {}, true)
+            data = await mysApi.getData('character_detail', { headers, ids: ids })
             if (!data.data) {
                 logger.mark('米游社查询失败')
                 return false
